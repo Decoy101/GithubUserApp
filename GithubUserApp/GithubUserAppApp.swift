f7c701e2710221a3a89a1481fa3c57b171a10efc
Decoy101 <00amangpt00@gmail.com>
2023-12-26 13:19:23 +0530

    Initial Commit

---
+++ GithubUserApp/GithubUserAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  GithubUserAppApp.swift
+//  GithubUserApp
+//
+//  Created by Aman Gupta on 26/12/23.
+//
+
+import SwiftUI
+
+@main
+struct GithubUserAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
